fce2555905d6b6b176bc255d35c4c552746d4a53
Dakota Dunn <dakotadunn5@gmail.com>
2022-02-11 23:57:14 -0800

    added axios to use api calls to get databreaches and all of it's data as well as for passwords using first 5 chars converted to hex

--- bin/index.js
+++ bin/index.js
@@ -5,15 +5,25 @@ const axios = require('axios').default;
 
 let arguments = yargs.argv;
 let pwn = arguments.pwn;
+let pass = arguments.pass;
 if (pwn){
     let pwnUrl = `https://haveibeenpwned.com/api/v2/breaches?domain=${pwn}`;
     axios.get(pwnUrl,{
 
     }).then((response) => {
-        let 
+        console.log(response.data);
     })
-    console.log(pwnUrl);
+    console.log(`You've chosen to look at ${pwnUrl} for breaches`);
+}else if(pass.length < 5){
+    console.log(`Please enter the first 5 hash chars`);
+}else if(pass){
+    let passUrl = `https://api.pwnedpasswords.com/range/${pass}`;
+    axios.get(passUrl,{
+
+    }).then((response) => {
+        console.log(response.data);
+    })
 }else{
-    return false;
+    console.log(`Use an argument`);
 }
 
